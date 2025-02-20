b4c70db33206ba1f1f2b546061085a0a92173e27
SAKTI BIMASENA <sbimasena@gmail.com>
2025-02-20 23:56:06 +0700

    feat: add board class and methods

---
+++ src/Board.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Board {
+    private int rows, cols;
+    private char[][] grid;
+
+    public Board(int rows, int cols){
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++){
+            Arrays.fill(grid[i], '.');
+        }
+    }
+
+    public boolean canPlaceShape(Shape shape, int x, int y, int rotation) {
+        List<int[]> shapeCoords = shape.getCoords(rotation);
+
+        for (int[] coordinates : shapeCoords){
+            int newRow = coordinates[0] + x;
+            int newCol = coordinates[1] + y;
+
+            if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols){
+                return false;
+            }
+
+            if (grid[newRow][newCol] != '.'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean placeShape(Shape shape, int x, int y, int rotation) {
+        if (canPlaceShape(shape, x, y, rotation)){
+            char letter = shape.getLetter();
+            List<int[]> shapeCoords = shape.getCoords(rotation);
+
+            for (int[] coordinates : shapeCoords){
+                int newRow = coordinates[0] + x;
+                int newCol = coordinates[1] + y;
+
+                grid[newRow][newCol] = letter;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean removeShape(Shape shape, int x, int y, int rotation) {
+        if (!canPlaceShape(shape, x, y, rotation)) {
+            return false; 
+        }
+        List<int[]> shapeCoords = shape.getCoords(rotation);
+
+        for (int[] coordinates : shapeCoords){
+            int newRow = coordinates[0] + x;
+            int newCol = coordinates[1] + y;
+
+            grid[newRow][newCol] = '.';
+        }
+        return true;
+    }
+}
